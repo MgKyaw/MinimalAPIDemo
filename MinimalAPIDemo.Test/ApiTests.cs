1151b85b9934818907ab5f93f2a5d3b032836a0f
MgKyaw <mgkyaw20112@gmail.com>
2025-09-16 20:26:38 +0700

    refactor: Add constructor and use global field for HttpClient

--- MinimalAPIDemo.Test/ApiTests.cs
+++ MinimalAPIDemo.Test/ApiTests.cs
@@ -5,13 +5,18 @@ namespace MinimalAPIDemo.Test;
 [TestClass]
 public class ApiTests
 {
+    private readonly HttpClient _httpClient;
+
+    public ApiTests()
+    {
+        var webApplicationFactory = new WebApplicationFactory<Program>();
+        _httpClient = webApplicationFactory.CreateDefaultClient();
+    }
+
     [TestMethod]
     public async Task DefaultRoute_ReturnsHelloWorld()
     {
-        var webApplicationFactory = new WebApplicationFactory<Program>();
-        var httpClient = webApplicationFactory.CreateDefaultClient();
-
-        var response = await httpClient.GetAsync("");
+        var response = await _httpClient.GetAsync("");
         var responseString = await response.Content.ReadAsStringAsync();
 
         Assert.AreEqual("Hello World!", responseString);
@@ -21,10 +26,7 @@ public class ApiTests
     [TestMethod]
     public async Task Sum_Returns16For10And6()
     {
-        var webApplicationFactory = new WebApplicationFactory<Program>();
-        var httpClient = webApplicationFactory.CreateDefaultClient();
-
-        var response = await httpClient.GetAsync("sum?n1=10&n2=6");
+        var response = await _httpClient.GetAsync("sum?n1=10&n2=6");
         var responseString = await response.Content.ReadAsStringAsync();
         var result = int.Parse(responseString);
 
