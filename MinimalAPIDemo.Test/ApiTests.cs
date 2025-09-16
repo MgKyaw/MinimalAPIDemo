c2c9fafa3178e7236fbb3a4774fce20a5bf34bfb
MgKyaw <mgkyaw20112@gmail.com>
2025-09-16 20:20:25 +0700

    feat: Add new test DefaultRoute_ReturnsHelloWorld

--- MinimalAPIDemo.Test/ApiTests.cs
+++ MinimalAPIDemo.Test/ApiTests.cs
@@ -1,10 +1,19 @@
-﻿namespace MinimalAPIDemo.Test;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MinimalAPIDemo.Test;
 
 [TestClass]
 public class ApiTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public async Task DefaultRoute_ReturnsHelloWorld()
     {
+        var webApplicationFactory = new WebApplicationFactory<Program>();
+        var httpClient = webApplicationFactory.CreateDefaultClient();
+
+        var response = await httpClient.GetAsync("");
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Assert.AreEqual("Hello World!", responseString);
     }
 }
